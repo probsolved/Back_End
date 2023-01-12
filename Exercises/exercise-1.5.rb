236aa557d3bd6a99a32f73123e395a99d321fae0
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2023-01-12 10:26:52 -0600

    Create exercise-1.5.rb

---
+++ Exercises/exercise-1.5.rb
@@ -0,0 +1,18 @@
+# Given the array of hashes:
+
+users = [
+    {
+        name: "John Doe",
+        age: 43
+    },
+    {
+        name: "Amy Singer",
+        age: 53
+    },
+    {
+        name: "Jimmy Lendricks",
+        age: 23
+    }
+]
+# Use a while loop to print the values of each hash such as "My name is ... and I am ...". If the first name starts with "Jimmy", print out "My name and age is confidential."
+
